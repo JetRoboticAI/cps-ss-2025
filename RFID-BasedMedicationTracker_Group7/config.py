317cbf44e31a30d9b448f43455f545883fe992f6
CynthiaaLee <54701734+CynthiaaLee@users.noreply.github.com>
2025-06-18 15:45:46 -0400

    Update config.py

--- RFID-BasedMedicationTracker_Group7/config.py
+++ RFID-BasedMedicationTracker_Group7/config.py
@@ -1,5 +1,5 @@
 # Replace with your actual bot token from @BotFather
-BOT_TOKEN = "7692518920:AAF43Zc8kSGit2Bf0SzGMx6jttSUbSW7QvM"
+BOT_TOKEN = ""
 
 # Replace with your Telegram chat ID (can be your user ID or a group ID)
-CHAT_ID = "8125505837"
+CHAT_ID = ""
